41fb1029d69c11865e33cabb295b724081c5bd83
qazplm123 <102525588@qq.com>
2018-03-03 13:56:42 +0800

    进度条插件的完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,22 @@
 /**
  * Created by zhangqianqian on 2018/3/2.
  */
+$(function(){
+    //禁用进度环
+    NProgress.configure({
+        showSpinner:false
+    })
+    //进度条加载功能
+
+
+    $(document).ajaxStart(function(){
+        NProgress.start();
+        console.log('ajax开始了')
+    })
+
+
+    $(document).ajaxStop(function(){
+        NProgress.done();
+        console.log('ajax结束了')
+    })
+})
